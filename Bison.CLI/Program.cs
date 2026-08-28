575f38437ebca5437ab8597f2e838581d67dabc5
Vralde <49454774+Vralde@users.noreply.github.com>
2026-08-28 13:00:26 +0200

    Uploaded initial project setup

---
+++ Bison.CLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileName = @"C:\Users\ninvr\Bison.CLI\bison_observe_cli_db.csv";
+
+        try
+        {
+            using StreamReader reader = new(fileName);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
