03512ad8e44218c15774d09d1ea47ecb07f94153
Daniel9915 <69625109+Daniel9915@users.noreply.github.com>
2026-08-28 13:54:54 +0200

    Can now read from csv file
    Co-authored-by: Frederik <fgte@itu.dk>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileName = @"bison_observe_cli_db.csv";
+
+        try
+        {
+            using StreamReader reader = new (fileName);
+            
+            //Skip first line
+            reader.ReadLine(); 
+
+            while (reader.Peek() >= 0)
+            {
+                //Need to check if there exists a line first
+
+                string line = reader.ReadLine();
+                string[] data = line.Split(",");
+                long seconds = long.Parse(data[2]);
+                DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(seconds);
+                Console.WriteLine(data[0] + " @ " + date + ": " + data[1]);
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
