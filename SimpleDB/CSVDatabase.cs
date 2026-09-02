24b83260f1c6c8b8d9f41d5912ac5c886cdea585
Rasmus Moses Appel <raap@itu.dk>
2026-09-02 14:53:44 +0200

    Create SImpleDB project to prepare for the rest of step 2.b week 2.
    Co-authored-by Valdemar Hedegaard <vahe.@tu.dk>

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,13 @@
+namespace SimpleDB;
+
+sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        
+    }
+    public void Store(T record)
+    {
+        
+    }
+}
