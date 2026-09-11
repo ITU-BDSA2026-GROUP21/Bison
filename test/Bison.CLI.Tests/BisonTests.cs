7148e0a9d469593e6e6616d57d235377eb72aaf6
rasmusmappel <rasmusmappel@gmail.com>
2026-09-11 19:11:10 +0200

    Add test file path handling
    - Test classes can now properly access the test data file templates, by copying them into their respective net8.0 directories before testing.
    - AI assistance was used to determine and discuss what approach to use for test data path handling.

--- test/Bison.CLI.Tests/BisonTests.cs
+++ test/Bison.CLI.Tests/BisonTests.cs
@@ -5,67 +5,66 @@ namespace Bison.CLI.Tests;
 public class BisonTests
 {
 
-    string observationPath = Path.Combine(AppContext.BaseDirectory, "testdata", "bison_observe_cli_db_test.csv");
+    string observationPath = Path.Combine(AppContext.BaseDirectory, "bison_observe_cli_db_test.csv");
 
-    string commentPath = Path.Combine(AppContext.BaseDirectory, "testdata", "bison_comment_cli_db_test.csv");
+    string commentPath = Path.Combine(AppContext.BaseDirectory, "bison_comment_cli_db_test.csv");
 
+    [Fact]
+    public void NonExistentObservationID()
+    {
+        // Arrange
+        CSVDatabase<ObservationRecord> observationDatabase = CSVDatabase<ObservationRecord>.getInstance(observationPath);
+
+        CSVDatabase<CommentRecord> commentDatabase = CSVDatabase<CommentRecord>.getInstance(commentPath);
+
+        bool argumentThrown = false;
+
+        // Act
+        try
+        {
+            UserInterface.comment(observationDatabase, commentDatabase, 9999999, "Test Comment");
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+            argumentThrown = true;
+        }
+
+        // Assert
+        Assert.True(argumentThrown, "Should throw an ArgumentException");
+    }
+
+    [Fact]
+    public void ObservationPrinting()
+    {
+        // Arrange
+        CSVDatabase<ObservationRecord> observationDatabase = CSVDatabase<ObservationRecord>.getInstance(observationPath);
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        UserInterface.PrintObservations(observationDatabase.Read(1));
+
+        string output = writer.ToString();
+
+        // Assert
+        Assert.Equal("ropf @ 08-01-23 12:09:20: A bird at DR Byen\r\n", output);
+    }
+
+    [Fact]
+    public void CommentPrinting()
+    {
+        // Arrange
+
+
+        // Act
+
+        // Assert
+    }
+
+
+    // Below here may be useful for integration tests
     /*
-        [Fact]
-        public void NonExistentObservationID()
-        {
-            // Arrange
-            CSVDatabase<ObservationRecord> observationDatabase = CSVDatabase<ObservationRecord>.getInstance(observationPath);
-
-            CSVDatabase<CommentRecord> commentDatabase = CSVDatabase<CommentRecord>.getInstance(commentPath);
-
-            bool argumentThrown = false;
-
-            // Act
-            try
-            {
-                UserInterface.comment(observationDatabase, commentDatabase, 9999999, "Test Comment");
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-                argumentThrown = true;
-            }
-
-            // Assert
-            Assert.True(argumentThrown, "Should throw an ArgumentException");
-        }
-
-        [Fact]
-        public void ObservationPrinting()
-        {
-            // Arrange
-            CSVDatabase<ObservationRecord> observationDatabase = CSVDatabase<ObservationRecord>.getInstance(observationPath);
-            using var writer = new StringWriter();
-            Console.SetOut(writer);
-
-            // Act
-            UserInterface.PrintObservations(observationDatabase.Read(1));
-
-            string output = writer.ToString();
-
-            // Assert
-            Assert.Equal("ropf @ 08/01/23 12:09:20: A bird at DR Byen\r\n", output);
-        }
-
-        [Fact]
-        public void CommentPrinting()
-        {
-            // Arrange
-
-
-            // Act
-
-            // Assert
-        }
-
-
-        // Below here may be useful for integration tests
-
         public CSVDatabase<ObservationRecord> CreateTestObservationDatabase()
         {
             //Copy base CSV, and create temporary test database and CSV file
@@ -102,5 +101,5 @@ public class BisonTests
         {
             DeleteCSV("../../testdata/test.csv");
             DeleteCSV("../../testdata/test2.csv");
-        } */
+        }*/
 }
