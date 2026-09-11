152e15bbccc6253da021860651ef761b880d43c8
rasmusmappel <rasmusmappel@gmail.com>
2026-09-12 00:39:02 +0200

    Extract observation and comment string construction as methods
    - I've removed some redundancy by extracting the construction of the observation record and comment record strings to their own toString methods.
    - At the same time, I fixed an issue where the date format used dashes (MM-dd-yy) when printing on my PC, whereas the Ubuntu runner on Github used slashes (MM/dd/yy). This has been fixed by applying CultureInfo.InvariantCulture in the toString calls when converting the Timestamp to a string. All date prints on all machines should now use slashes instead of dashes.

--- src/SimpleDB/ObservationRecord.cs
+++ src/SimpleDB/ObservationRecord.cs
@@ -1,9 +1,16 @@
+using System.Globalization;
+
 namespace SimpleDB;
 
 public record ObservationRecord
 {
-    public required string Author {get; set;}
-    public required string Observation {get; set;}
-    public required long Timestamp {get; set;}
-    public required int ID {get; set;}
+    public required string Author { get; set; }
+    public required string Observation { get; set; }
+    public required long Timestamp { get; set; }
+    public required int ID { get; set; }
+
+    public override string ToString()
+    {
+        return Author + " @ " + DateTimeOffset.FromUnixTimeSeconds(Timestamp).ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture) + ": " + Observation;
+    }
 }
