76478130d2ce6f0e80846e943b18adbd0a4e75d3
Thor Davis <tald@itu.dk>
2026-09-18 14:17:49 +0200

    Started work on the web API
    Co-authored-by: Frederik <fgte@itu.dk>

--- src/Bison.CSVDBService/Program.cs
+++ src/Bison.CSVDBService/Program.cs
@@ -1,6 +1,19 @@
+using SimpleDB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+IDatabaseRepository<ObservationRecord> observationDatabase = CSVDatabase<ObservationRecord>.getInstance("../../data/bison_observe_cli_db.csv");
+IDatabaseRepository<CommentRecord> commentDatabase = CSVDatabase<CommentRecord>.getInstance("../../data/bison_comment_cli_db.csv");
+
+app.MapGet("/observations", () => observationDatabase.Read());
+
+app.MapGet("/comments/{id}", (int ID) =>
+{
+    var result = commentDatabase.Read().Where(c => c.ObservationID == ID);
+
+    return result;
+});
 
 app.Run();
+public record Observation(string Author, string Message, long Timestamp);
